cb732dcd8cc2f5bb7d47ba0e113ebd17ec158328
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2026-01-30 18:51:46 +0100

    Add label field to db

--- qml/js/storage.js
+++ qml/js/storage.js
@@ -27,6 +27,15 @@ DB.migrations = [
         ')
         DB.makeTableSortable(tx, "_history", "seq")
     }],
+    [2, function(tx){
+        tx.executeSql('\
+            DROP VIEW IF EXISTS history;
+        ')
+        tx.executeSql('\
+            ALTER TABLE _history ADD COLUMN label TEXT DEFAULT "";
+        ')
+        DB.makeTableSortable(tx, "_history", "seq")
+    }],
 
     // add new versions here...
     //
@@ -42,7 +51,7 @@ function getHistoryEntries() {
     console.log("[storage] loading history entries")
 
     var q = DB.simpleQuery('\
-        SELECT rowid, date, tz, seq
+        SELECT rowid, date, tz, label, seq
         FROM history
         ORDER BY seq ASC
     ;', [], {notify: true})
@@ -53,7 +62,10 @@ function getHistoryEntries() {
     for (var i = 0; i < len; ++i) {
         var item = q.rows.item(i)
         entries.push({
-            rowid: item.rowid, date: item.date, tz: item.tz
+            rowid: item.rowid,
+            date: item.date,
+            tz: item.tz,
+            label: item.label,
         })
     }
 
@@ -77,17 +89,19 @@ function saveToHistory(date, tz) {
             rowid,
             date,
             tz,
+            label,
             seq
         ) VALUES (
             NULL,
             ?,
             ?,
+            "",
             NULL
         )
     ', [date, tz], {notify: true})
 
     var q3 = DB.simpleQuery('\
-        SELECT rowid, date, tz, seq
+        SELECT rowid, date, tz, label, seq
         FROM history
         ORDER BY seq DESC
         LIMIT 1
@@ -96,11 +110,25 @@ function saveToHistory(date, tz) {
     if (q3.rows.length > 0) {
         var item = q3.rows.item(0)
         return {
-            rowid: item.rowid, date: item.date, tz: item.tz
+            rowid: item.rowid,
+            date: item.date,
+            tz: item.tz,
+            label: item.label,
         }
     }
 
     return null
+}
+
+function setHistoryLabel(rowid, label) {
+    console.log("[storage] setting label for history entry", rowid, "to", label)
+
+    DB.simpleQuery('\
+        UPDATE history
+        SET label = ?
+        WHERE rowid = ?
+    ', [label, rowid],
+    {notify: true})
 }
 
 function moveHistoryItem(rowid, newIndex) {
