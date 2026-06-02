8d525ddbff23e3a4651328983beb735d3da01923
Smooth-E <smoothie@disroot.org>
2026-06-02 13:25:38 +0300

    Update WallClock every second

--- src/wallclock.cpp
+++ src/wallclock.cpp
@@ -12,7 +12,7 @@ WallClock::WallClock(QObject *parent)
 {
     QObject::connect(&m_timer, &QTimer::timeout, this, &WallClock::dateTimeChanged);
 
-    m_timer.setInterval(1000 * 60);
+    m_timer.setInterval(1000);
     m_timer.setSingleShot(false);
     m_timer.start();
 }
